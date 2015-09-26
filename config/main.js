772bedd032bf6a7cd405a7c541db6f3b71d8373b
Antti Kuosmanen <antti@seravo.fi>
2015-09-26 17:09:43 +0300

    Main loop implemented, config file added, executable added

---
+++ config/main.js
@@ -0,0 +1,6 @@
+/**
+ * The configuration file for this app
+ */
+module.exports = {
+  interval: 1000, // how often to check
+}
--- start.js
+++ start.js
@@ -1,5 +1,25 @@
 /**
- * Filename: start.js
- *
- * The entry point for this app. Starts the tracker daemon.
+ * The entry point for this project. Starts the tracker daemon.
  */
+
+// dependencies
+var _ = require('lodash');
+var config = require('app-config');
+
+// entry point
+var start = function() {
+  console.log('Starting the Floorball Live Tracker...');
+
+  // update the
+  setInterval(mainLoop, config.main.interval);
+
+}
+
+var mainLoop = function() {
+  console.log('Looping...');
+}
+
+// Automatically call the main function if we're the main module
+if (require.main === module) {
+  start();
+}
